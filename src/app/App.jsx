e6e81de92ab4961567bcf0efe90c901480012b85
Hirushi2001 <118071341+Hirushi2001@users.noreply.github.com>
2025-02-21 18:01:46 +0530

    all files are added

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css'
+import Login from '../common/pages/Login/Login'
+
+function App() {
+
+  return (
+    <>
+    <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ src/common/pages/Login/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Login.css';  // Import your CSS file
+
+function Login() {
+  return (
+    <div className="login-container">
+      <h2>Sign in to your account</h2>
+
+      {/* Form */}
+      <form>
+        <div>
+          <label>Email address</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <div>
+            <input
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+        >
+          Sign in
+        </button>
+      </form>
+
+      <p>
+        Don't have an account? <a href="#">Register</a>
+      </p>
+    </div>
+  )
+}
+
+export default Login
